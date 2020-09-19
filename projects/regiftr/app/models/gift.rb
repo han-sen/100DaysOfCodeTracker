805a6178819915fbb3cff4c8788811844d42178d
Michael Hansen <michaelandhansen@gmail.com>
2020-09-19 17:23:21 -0400

    all routes tested

--- projects/regiftr/app/models/gift.rb
+++ projects/regiftr/app/models/gift.rb
@@ -1,2 +1,6 @@
 class Gift < ApplicationRecord
+    validates :description, presence: true
+    validates :value, presence: true
+    validates :event, presence: true
+    validates :gift_giver, presence: true
 end
