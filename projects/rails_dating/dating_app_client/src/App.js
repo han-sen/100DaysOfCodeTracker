497d6a669080142cb388d303bf3807d7a78f6f14
Michael Hansen <michaelandhansen@gmail.com>
2020-09-21 17:17:47 -0400

    commit 2 set up CRA

---
+++ projects/rails_dating/dating_app_client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Daters from "./components/Daters";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <Daters />
+        </div>
+    );
+}
+
+export default App;
