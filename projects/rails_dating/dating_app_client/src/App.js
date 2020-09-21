ffe1377af559bf8cb236ba1c3f88213cff91c90d
Michael Hansen <michaelandhansen@gmail.com>
2020-09-21 19:07:37 -0400

    displaying users

--- projects/rails_dating/dating_app_client/src/App.js
+++ projects/rails_dating/dating_app_client/src/App.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Daters from "./components/Daters";
 import "./App.css";
 
 function App() {
+    const [daters, setDaters] = useState([]);
+    const [formInputs, updateFormInputs] = useState({
+        author: "",
+        content: "",
+        title: "",
+    });
+
+    const handleChange = (event) => {
+        const updateInput = Object.assign({}, formInputs, {
+            [event.target.id]: event.target.value,
+        });
+        updateFormInputs(updateInput);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch("http://localhost:3000/users", {
+                body: JSON.stringify(formInputs),
+                method: "POST",
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                },
+            });
+            const data = await response.json();
+            updateFormInputs({
+                author: "",
+                content: "",
+                title: "",
+            });
+            setDaters([data, ...daters]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getDaters = async () => {
+        try {
+            const response = await fetch("http://localhost:3000/users");
+            const data = await response.json();
+            setDaters(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    useEffect(() => {
+        (async function () {
+            await getDaters();
+        })();
+    }, []);
     return (
         <div className="App">
-            <Daters />
+            <header>
+                <h1>React on Rails Dating App</h1>
+            </header>
+            <Daters daters={daters} />
+            <footer>
+                <h2>Footer</h2>
+            </footer>
         </div>
     );
 }
--- projects/rails_dating/dating_app_client/src/components/Daters.js
+++ projects/rails_dating/dating_app_client/src/components/Daters.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React from "react";
 
-export default function Dater() {
-    return ()
+export default function Dater(props) {
+    return (
+        <section className="dating_wrap">
+            <div className="dating_left">
+                {props.daters
+                    .slice(0, props.daters.length / 2)
+                    .map((dater, i) => {
+                        return (
+                            <ul>
+                                <li key={i + dater.img}>
+                                    <img src={dater.img} alt={dater.name} />
+                                </li>
+                                <li key={i + dater.name}>{dater.name}</li>
+                                <li key={i + dater.starsign}>
+                                    {dater.starsign}
+                                </li>
+                            </ul>
+                        );
+                    })}
+            </div>
+            <div className="dating_center">
+                <h2>Matching Area</h2>
+            </div>
+            <div className="dating_right">
+                <ul>
+                    {props.daters
+                        .slice(props.daters.length / 2, props.daters.length)
+                        .map((dater, i) => {
+                            return (
+                                <ul>
+                                    <li key={i + dater.img}>
+                                        <img src={dater.img} alt={dater.name} />
+                                    </li>
+                                    <li key={i + dater.name}>{dater.name}</li>
+                                    <li key={i + dater.starsign}>
+                                        {dater.starsign}
+                                    </li>
+                                </ul>
+                            );
+                        })}
+                </ul>
+            </div>
+        </section>
+    );
 }
